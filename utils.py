959c2b0973ec84077e4fb9ecb72a195a001eacf1
Vigneshwar-KR <vigneshwar.kr@gmail.com>
2024-08-20 11:41:02 +0200

    Measurement update

--- utils.py
+++ utils.py
@@ -43,8 +43,10 @@ def process_images(folder_path):
             mid_group1_x, mid_group1_y = None, None
             mid_group2_x, mid_group2_y = None, None
             mid_group3_x, mid_group3_y = None, None
-            distance_1_2, distance_1_3 = None, None
-            distance_1_2_in_mm, distance_1_3_in_mm = None, None
+            distance_1_2_x, distance_1_2_y = None, None
+            distance_1_3_x, distance_1_3_y = None, None
+            distance_1_2_x_in_mm, distance_1_2_y_in_mm = None, None
+            distance_1_3_x_in_mm, distance_1_3_y_in_mm = None, None
 
             if circles is not None:
                 circles = np.round(circles[0, :]).astype("int")
@@ -80,12 +82,23 @@ def process_images(folder_path):
                     cv2.circle(image, mid_group2, 5, (0, 255, 0), -1)  # Green dot - midpoint of group 2
                     cv2.circle(image, mid_group3, 5, (0, 0, 255), -1)  # Red dot - midpoint of group 3
 
-                    distance_1_2 = math.sqrt((mid_group1_x - mid_group2_x)**2 + (mid_group1_y - mid_group2_y)**2)
-                    distance_1_3 = math.sqrt((mid_group1_x - mid_group3_x)**2 + (mid_group1_y - mid_group3_y)**2)
+                    # For values with directional info use this
+                    distance_1_2_x = mid_group2_x - mid_group1_x
+                    distance_1_2_y = mid_group2_y - mid_group1_y
+                    distance_1_3_x = mid_group3_x - mid_group1_x
+                    distance_1_3_y = mid_group3_y - mid_group1_y
+                    # # For absolute values use this
+                    # distance_1_2_x = abs(mid_group2_x - mid_group1_x)
+                    # distance_1_2_y = abs(mid_group2_y - mid_group1_y)
+                    # distance_1_3_x = abs(mid_group3_x - mid_group1_x)
+                    # distance_1_3_y = abs(mid_group3_y - mid_group1_y)
+
                     
                     conversion_factor = 0.07281  # our calculated value
-                    distance_1_2_in_mm = distance_1_2 * conversion_factor
-                    distance_1_3_in_mm = distance_1_3 * conversion_factor
+                    distance_1_2_x_in_mm = distance_1_2_x * conversion_factor
+                    distance_1_2_y_in_mm = distance_1_2_y * conversion_factor
+                    distance_1_3_x_in_mm = distance_1_3_x * conversion_factor
+                    distance_1_3_y_in_mm = distance_1_3_y * conversion_factor
 
                 else:
                     print(f"Unexpected number of circles in image {image_name}: {len(circles)}")
@@ -99,19 +112,25 @@ def process_images(folder_path):
                 'mid_group2_y [pixels]': mid_group2_y,
                 'mid_group3_x [pixels]': mid_group3_x,
                 'mid_group3_y [pixels]': mid_group3_y,
-                'Distance Group 1-2 [pixels]': distance_1_2,
-                'Distance Group 1-3 [pixels]': distance_1_3,
-                'Distance Group 1-2 [mm]': distance_1_2_in_mm,
-                'Distance Group 1-3 [mm]': distance_1_3_in_mm
+                'Distance Group 1-2 X [pixels]': distance_1_2_x,
+                'Distance Group 1-2 Y [pixels]': distance_1_2_y,
+                'Distance Group 1-3 X [pixels]': distance_1_3_x,
+                'Distance Group 1-3 Y [pixels]': distance_1_3_y,
+                'Distance Group 1-2 X [mm]': distance_1_2_x_in_mm,
+                'Distance Group 1-2 Y [mm]': distance_1_2_y_in_mm,
+                'Distance Group 1-3 X [mm]': distance_1_3_x_in_mm,
+                'Distance Group 1-3 Y [mm]': distance_1_3_y_in_mm
             })
-        # resized_image = resize_image(image, screen_width=800, screen_height=600)
-        # cv2.imshow(f'Hough Circle Detection - {image_name}', resized_image)
 
-        # cv2.waitKey(0)
+    # resized_image = resize_image(image, screen_width=800, screen_height=600)
+    # cv2.imshow(f'Hough Circle Detection - {image_name}', resized_image)
 
-        # cv2.destroyAllWindows()
+    # cv2.waitKey(0)
+
+    # cv2.destroyAllWindows()
+
+
     df = pd.DataFrame(all_data)
-
     output_path = os.path.join(folder_path, 'circle_detection_results.xlsx')
     df.to_excel(output_path, index=False)
     print(f"Data exported to {output_path}")
